79b4a8756245dd9fd91cb6b34f777dfca11e3fe3
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-06 15:45:47 +0700

    Local user log in

--- frontend/src/Components/LogIn/LogInContainer.tsx
+++ frontend/src/Components/LogIn/LogInContainer.tsx
@@ -4,6 +4,7 @@ import { Mutation, MutationFn } from "react-apollo";
 import { logIn, logInVariables } from "../../types/api";
 import { LOGIN_MUTATION } from "./LogInQueries";
 import { toast } from "react-toastify";
+import { LOG_USER_IN } from "../../sharedQueries.local";
 
 interface IState {
   username: string;
@@ -21,21 +22,28 @@ class LogInContainer extends React.Component<any, IState> {
   public render() {
     const { username, password } = this.state;
     return (
-      <LogInMutation
-        mutation={LOGIN_MUTATION}
-        variables={{ username, password }}
-        onError={() => toast.error("Wrong Username or Password")}
-      >
-        {(logInFn, { loading }) => (
-          <LogInPresenter
-            logInFn={logInFn}
-            username={username}
-            password={password}
-            onChangeHandler={this.onChangeHandler}
-            loading={loading}
-          />
+      <Mutation mutation={LOG_USER_IN}>
+        {logUserIn => (
+          <LogInMutation
+            mutation={LOGIN_MUTATION}
+            variables={{ username, password }}
+            onError={() => toast.error("Wrong Username or Password")}
+            onCompleted={({ logIn: { token } }) =>
+              logUserIn({ variables: { token } })
+            }
+          >
+            {(logInFn, { loading }) => (
+              <LogInPresenter
+                logInFn={logInFn}
+                username={username}
+                password={password}
+                onChangeHandler={this.onChangeHandler}
+                loading={loading}
+              />
+            )}
+          </LogInMutation>
         )}
-      </LogInMutation>
+      </Mutation>
     );
   }
   public onChangeHandler: React.ChangeEventHandler<
