24ff17f3d6580e09a8ff1807fcc028dd33991417
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-07 18:59:29 +0700

    Decorators and Exceptions on Card Queries

--- frontend/src/Routes/Feed/FeedPresenter.tsx
+++ frontend/src/Routes/Feed/FeedPresenter.tsx
@@ -3,6 +3,8 @@ import styled from "../../Styles/typed-components";
 
 const Container = styled.div``;
 
-const FeedPresenter: React.SFC<any> = () => <Container> Feed Login</Container>;
+const FeedPresenter: React.SFC<any> = () => (
+  <Container> Feed Login Sex</Container>
+);
 
 export default FeedPresenter;
