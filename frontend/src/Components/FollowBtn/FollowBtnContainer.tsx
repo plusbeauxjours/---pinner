a2a7d31d6243051ce706d4ee4bef331daa7e6e62
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-11 20:34:06 +0700

    Follow / Unfollow

--- frontend/src/Components/FollowBtn/FollowBtnContainer.tsx
+++ frontend/src/Components/FollowBtn/FollowBtnContainer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import FollowBtnPresenter from "./FollowBtnPresenter";
+import { Mutation, MutationFn } from "react-apollo";
+import { followUser, followUserVariables } from "../../types/api";
+import { FOLLOW_USER } from "./FollowBtnQueries";
+
+class FollowMutation extends Mutation<followUser, followUserVariables> {}
 
 interface IState {
   isFollowing: boolean;
 }
 
 class FollowBtnContainer extends React.Component<any, IState> {
+  public followUserFn: MutationFn;
   constructor(props) {
     super(props);
     this.state = {
@@ -14,11 +20,20 @@ class FollowBtnContainer extends React.Component<any, IState> {
   }
   public render() {
     const { isFollowing } = this.state;
+    const { userId } = this.props;
     return (
-      <FollowBtnPresenter
-        isFollowing={isFollowing}
-        toggleBtn={this.toggleBtn}
-      />
+      <FollowMutation
+        mutation={FOLLOW_USER}
+        variables={{ userId: parseInt(userId, 10) }}
+        onCompleted={this.toggleBtn}
+      >
+        {followUserFn => (
+          <FollowBtnPresenter
+            isFollowing={isFollowing}
+            toggleBtn={followUserFn}
+          />
+        )}
+      </FollowMutation>
     );
   }
   public toggleBtn = () => {
