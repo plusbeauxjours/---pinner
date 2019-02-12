6063d5a3d9f1f0b961ea205b7f2c7a524842ac14
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-12 17:52:59 +0700

    Profile 1/3

---
+++ frontend/src/Routes/Profile/ProfileQueries.ts
@@ -0,0 +1,27 @@
+import { gql } from "apollo-boost";
+import { CARD_FRAGMENT } from "src/sharedQueries";
+
+export const GET_USER = gql`
+  query userProfile($username: String!) {
+    userProfile(username: $username) {
+      user {
+        id
+        username
+        firstName
+        lastName
+        profile {
+          bio
+          avatar
+          website
+          postCount
+          followersCount
+          followingCount
+        }
+        cards {
+          ...CardParts
+        }
+      }
+    }
+  }
+  ${CARD_FRAGMENT}
+`;
