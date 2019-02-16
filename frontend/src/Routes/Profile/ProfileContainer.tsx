077a3773ef07e77e8e472300c79a8cf13fc63cfa
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-16 17:18:50 +0700

    Modal + Log Out

--- frontend/src/Routes/Profile/ProfileContainer.tsx
+++ frontend/src/Routes/Profile/ProfileContainer.tsx
@@ -1,33 +1,60 @@
 import React from "react";
 import ProfilePresenter from "./ProfilePresenter";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import { userProfile, userProfileVariables } from "src/types/api";
 import { GET_USER } from "./ProfileQueries";
 import { withRouter, RouteComponentProps } from "react-router";
+import { LOG_USER_OUT } from "src/sharedQueries.local";
 
 class ProfileQuery extends Query<userProfile, userProfileVariables> {}
+class LogOutMutation extends Mutation {}
 
 interface IProps extends RouteComponentProps<any> {}
 
-class ProfileContainer extends React.Component<IProps> {
+interface IState {
+  modalOpen: boolean;
+}
+
+class ProfileContainer extends React.Component<IProps, IState> {
+  public state = {
+    modalOpen: false
+  };
   public render() {
     const {
       match: {
         params: { username }
       }
     } = this.props;
+    const { modalOpen } = this.state;
     return (
-      <ProfileQuery
-        query={GET_USER}
-        variables={{ username }}
-        fetchPolicy="network-only"
-      >
-        {({ data, loading }) => (
-          <ProfilePresenter loading={loading} data={data} />
+      <LogOutMutation mutation={LOG_USER_OUT}>
+        {logUserOutFn => (
+          <ProfileQuery
+            query={GET_USER}
+            variables={{ username }}
+            fetchPolicy="network-only"
+          >
+            {({ data, loading }) => (
+              <ProfilePresenter
+                logUserOutFn={logUserOutFn}
+                toggleModal={this.toggleModal}
+                loading={loading}
+                data={data}
+                modalOpen={modalOpen}
+              />
+            )}
+          </ProfileQuery>
         )}
-      </ProfileQuery>
+      </LogOutMutation>
     );
   }
+  public toggleModal = () => {
+    this.setState(state => {
+      return {
+        modalOpen: !state.modalOpen
+      };
+    });
+  };
 }
 
 export default withRouter(ProfileContainer);
--- frontend/src/Routes/Profile/ProfilePresenter.tsx
+++ frontend/src/Routes/Profile/ProfilePresenter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "../../Styles/typed-components";
+import styled, { keyframes } from "../../Styles/typed-components";
 import Wrapper from "../../Components/Wrapper";
 import Loader from "src/Components/Loader";
 import Avatar from "src/Components/Avatar";
@@ -8,6 +8,7 @@ import CardGrid from "src/Components/CardGrid";
 import { Link } from "react-router-dom";
 import Button from "../../Components/Button";
 import FollowBtn from "src/Components/FollowBtn";
+import { Gear } from "../../Icons";
 
 const SWrapper = styled(Wrapper)`
   width: 45%;
@@ -65,7 +66,76 @@ const UsernameRow = styled.div`
   }
 `;
 
-const ProfilePresenter: React.SFC<any> = ({ data, loading }) => {
+const ModalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+`;
+
+const GearContainer = styled.span`
+  margin-left: 15px;
+  cursor: pointer;
+`;
+
+const ModalOverlay = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const ModalAnimation = keyframes`
+	  from{
+	    opacity:0;
+	    transform:scale(1.1);
+	  }
+	  to{
+	    opacity:1;
+	    transform:none;
+	  }
+	`;
+
+const Modal = styled.div`
+  background-color: white;
+  width: 30%;
+  border-radius: 12px;
+  z-index: 5;
+  animation: ${ModalAnimation} 0.1s linear;
+`;
+
+const ModalLink = styled.div`
+  text-align: center;
+  min-height: 50px;
+  width: 100%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  :not(:last-child) {
+    border-bottom: 1px solid #efefef;
+  }
+`;
+
+interface IProps {
+  data?: any;
+  loading: boolean;
+  modalOpen: boolean;
+  toggleModal: () => void;
+  logUserOutFn: () => void;
+}
+
+const ProfilePresenter: React.SFC<IProps> = ({
+  data,
+  loading,
+  modalOpen,
+  toggleModal,
+  logUserOutFn
+}) => {
   if (loading) {
     return <Loader />;
   } else if (!loading && data) {
@@ -74,6 +144,15 @@ const ProfilePresenter: React.SFC<any> = ({ data, loading }) => {
     } = data;
     return (
       <>
+        {modalOpen && (
+          <ModalContainer>
+            <ModalOverlay onClick={toggleModal} />
+            <Modal>
+              <ModalLink onClick={logUserOutFn}>Log Out</ModalLink>
+              <ModalLink onClick={toggleModal}>Cancel</ModalLink>
+            </Modal>
+          </ModalContainer>
+        )}
         <SWrapper>
           <Header>
             <Avatar size="lg" url={user.profile.avatar} />
@@ -81,9 +160,18 @@ const ProfilePresenter: React.SFC<any> = ({ data, loading }) => {
               <UsernameRow>
                 <Username>{user.username}</Username>
                 {user.profile.isSelf ? (
-                  <Link to="/edit-profile">
-                    <Button text="Edit Profile" inverted={true} />
-                  </Link>
+                  <>
+                    <Link to="/edit-profile">
+                      <Button
+                        text="Edit Profile"
+                        inverted={true}
+                        active={true}
+                      />
+                    </Link>
+                    <GearContainer onClick={toggleModal}>
+                      <Gear />
+                    </GearContainer>
+                  </>
                 ) : (
                   <FollowBtn
                     isFollowing={user.profile.isFollowing}
