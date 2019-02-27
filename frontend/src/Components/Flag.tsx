c8ec52a074fe68952009729ed2f28cd848f572a2
plusbeauxjours <plusbeauxjours@gmail.com>
2019-02-27 07:02:16 +0700

    Shadow on Flag

--- frontend/src/Components/Flag.tsx
+++ frontend/src/Components/Flag.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "src/Styles/typed-components";
 
-const Container = styled.img<IProps>`
+const Flag = styled.img<IProps>`
   height: ${props => {
     if (props.size === "md") {
       return "50px";
@@ -24,10 +24,68 @@ const Container = styled.img<IProps>`
       return "30px";
     }
   }};
+  display: flex;
   background-position: center center;
   background-size: 100%;
   border-radius: 50%;
   opacity: 0.8;
+`;
+
+const Overlay = styled.div<IProps>`
+  height: ${props => {
+    if (props.size === "md") {
+      return "50px";
+    } else if (props.size === "sm") {
+      return "30px";
+    } else if (props.size === "lg") {
+      return "150px";
+    } else {
+      return "30px";
+    }
+  }};
+  width: ${props => {
+    if (props.size === "md") {
+      return "25px";
+    } else if (props.size === "sm") {
+      return "15px";
+    } else if (props.size === "lg") {
+      return "75px";
+    } else {
+      return "15px";
+    }
+  }};
+  color: white;
+  z-index: 1;
+  background: rgba(0, 0.1, 0, 0.2);
+  background-position: center center;
+  cursor: pointer;
+  transition: opacity 0.2s ease-in-out;
+  border-bottom-left-radius: ${props => {
+    if (props.size === "md") {
+      return "100px";
+    } else if (props.size === "sm") {
+      return "60px";
+    } else if (props.size === "lg") {
+      return "300px";
+    } else {
+      return "60px";
+    }
+  }};
+  border-top-left-radius: ${props => {
+    if (props.size === "md") {
+      return "100px";
+    } else if (props.size === "sm") {
+      return "60px";
+    } else if (props.size === "lg") {
+      return "300px";
+    } else {
+      return "60px";
+    }
+  }};
+  margin-bottom: 15px;
+  &:hover {
+    opacity: 0.5;
+  }
 `;
 
 interface IProps {
@@ -37,8 +95,10 @@ interface IProps {
   className?: string;
 }
 
-const Avatar: React.SFC<IProps> = ({ className, countrycode, size }) => {
-  return <Container className={className} src={countrycode} size={size} />;
-};
+const Avatar: React.SFC<IProps> = ({ className, countrycode, size }) => (
+  <Overlay size={size}>
+    <Flag className={className} src={countrycode} size={size} />
+  </Overlay>
+);
 
 export default Avatar;
