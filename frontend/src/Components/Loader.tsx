f3de4f09f08ac499c15f6f5c10f8542f5115b3d0
plusbeauxjours <plusbeauxjours@gmail.com>
2019-03-27 20:47:34 +0900

    Spinner Changed

--- frontend/src/Components/Loader.tsx
+++ frontend/src/Components/Loader.tsx
@@ -1,77 +1,34 @@
+import LoadingOverlay from "react-loading-overlay";
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Container = styled.div`
-  margin-top: 100px;
   display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const loaderAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  
-  25% {
-    transform: rotate(180deg);
-  }
-  
-  50% {
-    transform: rotate(180deg);
-  }
-  
-  75% {
-    transform: rotate(360deg);
-  }
-  
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-const loaderInnerAnimation = keyframes`
-  0% {
-    height: 0%;
-  }
-  25% {
-    height: 0%;
-  }
-  
-  50% {
-    height: 100%;
-  }
-  
-  75% {
-    height: 100%;
-  }
-  
-  100% {
-    height: 0%;
-  }
-`;
-
-const Loader = styled.div`
-  display: inline-block;
-  width: 30px;
-  height: 30px;
-  position: relative;
-  border: 4px solid grey;
-  top: 50%;
-  animation: ${loaderAnimation} 2s linear;
-`;
-
-const LoaderInner = styled.div`
-  display: inline-block;
   width: 100%;
-  background-color: grey;
-  animation: ${loaderInnerAnimation} 2s linear;
+  height: 100%;
+  position: fixed;
+  justify-content: center;
+  align-items: center;
+  text-align: inline;
 `;
 
-export default () => (
-  <Container>
-    <Loader>
-      <LoaderInner />
-    </Loader>
-  </Container>
-);
+interface IProps {
+  text?: string;
+}
+
+class Loader extends React.Component<IProps> {
+  public render() {
+    return (
+      <Container>
+        <LoadingOverlay
+          active={true}
+          spinner={true}
+          fadeSpeed={500}
+          text="Loading"
+        />
+      </Container>
+    );
+  }
+}
+
+export default Loader;
