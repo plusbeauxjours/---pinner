9646eda5e9cfff525ce7afdea1bdd4aa2d1f55e0
plusbeauxjours <plusbeauxjours@gmail.com>
2019-04-02 21:48:46 +0900

    MatchComponents

--- frontend/src/Components/Match/MatchContainer.tsx
+++ frontend/src/Components/Match/MatchContainer.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import MatchPresenter from "./MatchPresenter";
 
-class MatchContainer extends React.Component {
+interface IState {
+  data: any;
+}
+
+class MatchContainer extends React.Component<any, IState> {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [0, 1, 2, 3, 4]
+    };
   }
-  public render = () => {
-    return <MatchPresenter />;
-  };
+  public render() {
+    const { data } = this.state;
+    const box = data.map((item, i) => {
+      return <MatchPresenter key={i} no={i} />;
+    });
+    return <div className="app">{box}</div>;
+  }
 }
 
 export default MatchContainer;
