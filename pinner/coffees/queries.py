ff5f703b0ef24e4c661528efd86637e019382066
plusbeauxjours <plusbeauxjours@gmail.com>
2019-04-11 13:27:20 +0900

    Bugfix

--- pinner/coffees/queries.py
+++ pinner/coffees/queries.py
@@ -10,16 +10,17 @@ def resolve_get_coffee(self, info, **kwargs):
     user = info.context.user
     cityName = kwargs.get('cityName')
 
-    city = location_models.objects.get(city_name=cityName)
-    followings = user.followed_by.all()
+    city = location_models.City.objects.get(city_name=cityName)
+    profile = user.profile
+    followings = profile.followed_by.all()
     print(followings)
 
     everyone = city.coffee.filter(target='everyone')
-    nationality = city.coffee.filter(target='nationality', host__nationality=user.natinality)
-    gender = city.coffee.filter(target='gender', host__gender=user.gender)
-    followers = city.coffee.filter(target='followers', host__in=followings)
+    nationality = city.coffee.filter(target='nationality', host__profile__nationality=profile.nationality)
+    gender = city.coffee.filter(target='gender', host__profile__gender=profile.gender)
+    followers = city.coffee.filter(target='followers', host__profile__in=followings)
 
-    combined = following_cards.union(city_cards).union(my_cards).order_by(
+    combined = everyone.union(nationality).union(gender).union(followers).order_by(
         '-created_at')
 
     return types.GetCoffeeResponse(coffees=combined)
