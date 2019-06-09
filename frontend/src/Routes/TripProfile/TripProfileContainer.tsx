a7b0027390499c550d266b342f826932ebf768a9
plusbeauxjours <plusbeauxjours@gmail.com>
2019-06-09 15:27:08 +0900

    TripProfile Search

--- frontend/src/Routes/TripProfile/TripProfileContainer.tsx
+++ frontend/src/Routes/TripProfile/TripProfileContainer.tsx
@@ -19,14 +19,18 @@ class NearCitiesQuery extends Query<NearCities, NearCitiesVariables> {}
 interface IProps extends RouteComponentProps<any> {}
 
 interface IState {
+  search: string;
   cityName: string;
   cityPhoto: string;
   countryName: string;
   startDate: moment.Moment | null;
   endDate: moment.Moment | null;
+  usersBeforeList: any;
+  usersBeforeActiveId: number;
 }
 
 class TripProfileContainer extends React.Component<IProps, IState> {
+  public data;
   public fetchMore;
   constructor(props) {
     super(props);
@@ -35,18 +39,33 @@ class TripProfileContainer extends React.Component<IProps, IState> {
       props.history.goBack();
     }
     this.state = {
+      search: "",
       cityName: state.cityName,
       cityPhoto: state.cityPhoto,
       countryName: state.countryName,
       startDate: state.tripStartDate,
-      endDate: state.tripEndDate
+      endDate: state.tripEndDate,
+      usersBeforeList: [],
+      usersBeforeActiveId: null
     };
   }
-  public componentDidMount() {
-    console.log("goodmorning");
+  public componentDidUpdate(prevProps) {
+    const newProps = this.props;
+    if (prevProps.match.params.cityName !== newProps.match.params.cityName) {
+      this.setState({ search: "", usersBeforeList: [] });
+    }
   }
   public render() {
-    const { cityName, cityPhoto, countryName, startDate, endDate } = this.state;
+    const {
+      search,
+      cityName,
+      cityPhoto,
+      countryName,
+      startDate,
+      endDate,
+      usersBeforeList,
+      usersBeforeActiveId
+    } = this.state;
     return (
       <NearCitiesQuery query={NEAR_CITIES} variables={{ cityName }}>
         {({ data: nearCitiesData, loading: nearCitiesLoading }) => {
@@ -55,25 +74,109 @@ class TripProfileContainer extends React.Component<IProps, IState> {
               query={TRIP_PROFILE}
               variables={{ cityName, startDate, endDate }}
             >
-              {({ data: profileDate, loading: profileLoading }) => (
-                <TripProfilePresenter
-                  cityName={cityName}
-                  cityPhoto={cityPhoto}
-                  countryName={countryName}
-                  startDate={startDate}
-                  endDate={endDate}
-                  profileDate={profileDate}
-                  profileLoading={profileLoading}
-                  nearCitiesData={nearCitiesData}
-                  nearCitiesLoading={nearCitiesLoading}
-                />
-              )}
+              {({ data: profileDate, loading: profileLoading }) => {
+                this.data = profileDate;
+                return (
+                  <TripProfilePresenter
+                    cityName={cityName}
+                    cityPhoto={cityPhoto}
+                    countryName={countryName}
+                    startDate={startDate}
+                    endDate={endDate}
+                    profileDate={profileDate}
+                    profileLoading={profileLoading}
+                    nearCitiesData={nearCitiesData}
+                    nearCitiesLoading={nearCitiesLoading}
+                    search={search}
+                    usersBeforeList={usersBeforeList}
+                    usersBeforeActiveId={usersBeforeActiveId}
+                    onKeyDown={this.onKeyDown}
+                    onClick={this.onClick}
+                    onBlur={this.onBlur}
+                    onChange={this.onChange}
+                  />
+                );
+              }}
             </TripProfileQuery>
           );
         }}
       </NearCitiesQuery>
     );
   }
+  public onChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+    const {
+      target: { value }
+    } = event;
+    const {
+      tripProfile: { usersBefore = null }
+    } = this.data;
+    const beforeSearch = (list, text) =>
+      list.filter(i =>
+        i.actor.profile.username.toLowerCase().includes(text.toLowerCase())
+      );
+    const usersBeforeList = beforeSearch(usersBefore, value);
+    this.setState({
+      search: value,
+      usersBeforeList,
+      activeId: 0
+    } as any);
+  };
+  public onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const { keyCode } = event;
+    const { usersBeforeActiveId, usersBeforeList } = this.state;
+    const { history } = this.props;
+
+    const { tripProfile: { usersBefore = null } = {} } = this.data;
+
+    if (keyCode === 13 && (usersBeforeList.length || usersBefore)) {
+      {
+        usersBeforeList.length
+          ? history.push({
+              pathname: `/${
+                usersBeforeList[usersBeforeActiveId].actor.profile.username
+              }`
+            })
+          : history.push({
+              pathname: `/${
+                usersBefore[usersBeforeActiveId].actor.profile.username
+              }`
+            });
+      }
+      this.setState({
+        usersBeforeActiveId: 0
+      });
+    } else if (keyCode === 38) {
+      if (usersBeforeActiveId === 0) {
+        return;
+      }
+      this.setState({
+        usersBeforeActiveId: usersBeforeActiveId - 1
+      });
+    } else if (keyCode === 40) {
+      if (usersBeforeList.length) {
+        if (usersBeforeActiveId === usersBeforeList.length - 1) {
+          return;
+        }
+      } else {
+        if (usersBeforeActiveId === usersBefore.length - 1) {
+          return;
+        }
+      }
+      this.setState({
+        usersBeforeActiveId: usersBeforeActiveId + 1
+      });
+    }
+  };
+  public onClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    this.setState({
+      usersBeforeActiveId: 0
+    });
+  };
+  public onBlur: React.MouseEventHandler<HTMLDivElement> = () => {
+    this.setState({
+      usersBeforeActiveId: null
+    });
+  };
 }
 
 export default withRouter(TripProfileContainer);
--- frontend/src/Routes/TripProfile/TripProfilePresenter.tsx
+++ frontend/src/Routes/TripProfile/TripProfilePresenter.tsx
@@ -7,7 +7,6 @@ import Loader from "../../Components/Loader";
 import Avatar from "../../Components/Avatar";
 import Bold from "../../Components/Bold";
 import moment = require("moment");
-import LocationGrid from "src/Components/LocationGrid";
 import Weather from "src/Components/Weather";
 import AvatarGrid from "../../Components/AvatarGrid";
 import UserHeader from "../../Components/UserHeader";
@@ -16,21 +15,9 @@ const SWrapper = styled(Wrapper)`
   z-index: 1;
 `;
 
-const Container = styled.div`
-  border-bottom: 4px;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  -webkit-box-flex: 0;
-  flex: 0 0 auto;
-  height: 280px;
-  padding: 15px;
-`;
-
 const PHeader = styled.header`
   display: flex;
   padding: 40px 15px 40px 15px;
-
   @media screen and (max-width: 600px) {
     justify-content: center;
     flex-wrap: wrap;
@@ -41,35 +28,6 @@ const Username = styled.span`
   text-align: center;
   font-size: 22px;
   font-weight: 100;
-`;
-
-const CityPhoto = styled.img`
-  margin-bottom: 10px;
-  display: flex;
-  width: 200px;
-  height: 200px;
-  background-size: cover;
-  border-radius: 3px;
-  z-index: 1;
-  object-fit: cover;
-`;
-
-const CountryName = styled(Bold)`
-  position: absolute;
-  display: flex;
-  z-index: 5;
-  font-size: 40px;
-  font-family: "Qwigley";
-  font-weight: 200;
-  pointer-events: none;
-`;
-
-const CountryContainer = styled.div`
-  margin-right: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
 `;
 
 const InfoRow = styled.span``;
@@ -86,7 +44,11 @@ const SSText = styled(Bold)`
 
 const Title = styled.div`
   display: flex;
+  justify-content: space-between;
   margin-top: 10px;
+  @media screen and (max-width: 935px) {
+    margin-left: 10px;
+  }
 `;
 
 const SmallTitle = styled(Title)`
@@ -94,31 +56,13 @@ const SmallTitle = styled(Title)`
   align-items: center;
 `;
 
-const Box = styled.div`
-  width: 905px;
-  display: flex;
-  overflow-x: auto;
-  -ms-overflow-style: -ms-autohiding-scrollbar;
-  ::-webkit-scrollbar {
-    height: 6px;
-  }
-  ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    border-radius: 10px;
-    background-color: ${props => props.theme.bgColor};
-  }
-
-  ::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
-    background-color: ${props => props.theme.greyColor};
-  }
-`;
-
 const GreyLine = styled.div`
   margin-top: 10px;
   margin-bottom: 10px;
   border-bottom: 1px solid grey;
+  @media screen and (max-width: 935px) {
+    margin: 0 10px 0 10px;
+  }
 `;
 
 const SmallGreyLine = styled(GreyLine)`
@@ -130,6 +74,9 @@ const CAvatar = styled(Avatar)`
   height: 300px;
   width: 300px;
   margin-right: 20px;
+  @media screen and (max-width: 600px) {
+    margin-right: 0px;
+  }
 `;
 
 const UserContainer = styled.div`
@@ -141,33 +88,127 @@ const UserContainer = styled.div`
   }
 `;
 
-const UserRow = styled.div`
+const UserRow = styled.div<ITheme>`
   display: grid;
   flex-direction: row;
-  height: 50px;
   grid-template-columns: 4fr 1fr;
   padding: 0 5px 0 5px;
   grid-gap: 15px;
   align-items: center;
   cursor: pointer;
   transition: background-color 0.2s ease-in-out;
+  background-color: ${props => (props.active ? "grey" : null)};
   &:hover {
     background-color: grey;
   }
-  border-top: 1px solid grey;
+  &:not(:last-child) {
+    border-bottom: 1px solid grey;
+  }
 `;
+
+// const LocationRow = styled(UserRow)`
+//   width: 300px;
+//   height: 50px;
+//   margin-top: 15px;
+//   margin-bottom: 15px;
+//   border-top: 1px solid grey;
+// `;
 
 const UserNameRow = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  margin-bottom: 10px;
 `;
 
 const AvatarContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+const Input = styled.input`
+  width: 215px;
+  border: 0;
+  border: ${props => props.theme.boxBorder};
+  background-color: ${props => props.theme.bgColor};
+  border-radius: 3px;
+  padding: 5px;
+  color: white;
+  font-size: 12px;
+  &::placeholder {
+    color: ${props => props.theme.greyColor};
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+const HeaderText = styled(Bold)`
+  display: flex;
+`;
+
+const HeaderColumn = styled.div`
+  margin-left: 15px;
+`;
+
+const SAvatar = styled(Avatar)`
+  border-radius: 3px;
+  height: 45px;
+  width: 45px;
+`;
+
+const Location = styled.span`
+  display: flex;
+  margin-top: 5px;
+  display: block;
+  font-size: 12px;
+  font-weight: 200;
+`;
+
+const Container = styled.div`
+  -webkit-box-flex: 0;
+  padding: 15px;
+`;
+
+const Box = styled.div`
+  max-width: 905px;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-rows: repeat(3, 50px);
+  grid-auto-columns: 400px;
+  column-gap: 10px;
+  overflow-x: auto;
+  padding-bottom: 15px;
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  ::-webkit-scrollbar {
+    height: 6px;
+  }
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: ${props => props.theme.bgColor};
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+    background-color: ${props => props.theme.greyColor};
+  }
+`;
+
+const SeeAll = styled.p`
+  font-size: 12px;
+  font-weight: 100;
+  cursor: pointer;
+`;
+
+interface ITheme {
+  active?: string;
+}
 
 interface IProps {
   cityName: string;
@@ -180,6 +221,14 @@ interface IProps {
   profileLoading: boolean;
   nearCitiesData?: any;
   nearCitiesLoading: boolean;
+
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  search: string;
+  usersBeforeList: any;
+  onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+  onClick: any;
+  onBlur: any;
+  usersBeforeActiveId: number;
 }
 
 const TripProfilePresenter: React.SFC<IProps> = ({
@@ -194,7 +243,15 @@ const TripProfilePresenter: React.SFC<IProps> = ({
   } = {},
   profileLoading,
   nearCitiesData: { nearCities: { cities: nearCities = null } = {} } = {},
-  nearCitiesLoading
+  nearCitiesLoading,
+
+  onChange,
+  search,
+  usersBeforeList,
+  onKeyDown,
+  onClick,
+  onBlur,
+  usersBeforeActiveId
 }) => {
   if (profileLoading) {
     return <Loader />;
@@ -222,11 +279,23 @@ const TripProfilePresenter: React.SFC<IProps> = ({
                 <Username>
                   From {startDate} To {endDate}
                 </Username>
+                <Input
+                  placeholder="Search"
+                  onChange={onChange}
+                  value={search}
+                  onKeyDown={onKeyDown}
+                  onClick={onClick}
+                  onBlur={onBlur}
+                />
               </UserNameRow>
-              {usersBefore &&
-                usersBefore.map(user => (
-                  <>
-                    <UserRow key={user.actor.profile.id}>
+              {usersBeforeList.length !== 0 &&
+                usersBeforeList.map((user, index) => {
+                  let active;
+                  if (index === usersBeforeActiveId) {
+                    active = "active";
+                  }
+                  return (
+                    <UserRow key={index} active={active}>
                       <Link to={`/${user.actor.profile.username}`}>
                         <UserHeader
                           username={user.actor.profile.username}
@@ -239,8 +308,32 @@ const TripProfilePresenter: React.SFC<IProps> = ({
                         />
                       </Link>
                     </UserRow>
-                  </>
-                ))}
+                  );
+                })}
+              {usersBeforeList.length === 0 &&
+                !search &&
+                usersBefore &&
+                usersBefore.map((user, index) => {
+                  let active;
+                  if (index === usersBeforeActiveId) {
+                    active = "active";
+                  }
+                  return (
+                    <UserRow key={index} active={active}>
+                      <Link to={`/${user.actor.profile.username}`}>
+                        <UserHeader
+                          username={user.actor.profile.username}
+                          currentCity={user.actor.profile.currentCity.cityName}
+                          currentCountry={
+                            user.actor.profile.currentCity.country.countryName
+                          }
+                          avatar={user.actor.profile.avatar}
+                          size={"sm"}
+                        />
+                      </Link>
+                    </UserRow>
+                  );
+                })}
             </UserContainer>
           </PHeader>
 
@@ -255,38 +348,32 @@ const TripProfilePresenter: React.SFC<IProps> = ({
           ) : null}
           <GreyLine />
           <Title>
-            <SText text={`WHERE ${cityName} IS`} />
-          </Title>
-          <Container>
-            <CountryContainer>
-              <Link to={`/country/${city.country.countryName}`}>
-                <CityPhoto src={city.country.countryPhoto} />
-              </Link>
-              <CountryName text={city.country.countryName} />
-              {console.log(city)}
-            </CountryContainer>
-            <Box>
-              {!nearCitiesLoading && nearCities ? (
-                <LocationGrid cities={nearCities} type={"city"} />
-              ) : (
-                <Loader />
-              )}
-            </Box>
-          </Container>
-          <GreyLine />
-          <Title>
             <SText text={"NEAR CITIES"} />
+            {/* <Link to={`${history.location.pathname}/usersbefore`}> */}
+            <SeeAll>SEE ALL</SeeAll>
+            {/* </Link> */}
           </Title>
           <Container>
             <Box>
-              {!nearCitiesLoading && nearCities ? (
-                <LocationGrid cities={nearCities} type={"city"} />
+              {nearCities && !nearCitiesLoading && nearCities.length !== 0 ? (
+                nearCities.map(nearCity => (
+                  <UserRow key={nearCity.id}>
+                    <Link to={`/city/${nearCity.cityName}`}>
+                      <Header>
+                        <SAvatar size={"sm"} url={nearCity.cityPhoto} />
+                        <HeaderColumn>
+                          <HeaderText text={nearCity.cityName} />
+                          <Location>{nearCity.country.countryName}</Location>
+                        </HeaderColumn>
+                      </Header>
+                    </Link>
+                  </UserRow>
+                ))
               ) : (
                 <Loader />
               )}
             </Box>
           </Container>
-          <GreyLine />
         </SWrapper>
       </>
     );
