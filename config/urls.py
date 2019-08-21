404f87a4e4ec58c22e55d9a263ef8865a01faacb
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-22 05:14:06 +0900

    commit to heroku

--- config/urls.py
+++ config/urls.py
@@ -10,12 +10,6 @@ from graphene_file_upload.django import FileUploadGraphQLView
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
