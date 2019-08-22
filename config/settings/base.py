2cfb7529d7e793d896a7117f56a77084937512ae
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-23 04:15:32 +0900

    commit to heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,6 +160,7 @@ MIDDLEWARE = [
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
