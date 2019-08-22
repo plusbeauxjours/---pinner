bf606c6140c5bdbc9881164bbc73f206344180be
plusbeauxjours <plusbeauxjours@gmail.com>
2019-08-23 02:24:28 +0900

    commit to heroku

--- config/settings/test.py
+++ config/settings/test.py
@@ -9,7 +9,7 @@ from .base import env
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY", default="wE6u8XAuZRlYirSZrrTz5R0AkXmNlfy57COuo55f4czPI21UBX4cClLMZsUGVlRY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
